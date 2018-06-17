a0673eba03a5cb0507ad324c66cf674825f97d34
Gisel Hernández <gisel.hv@gmail.com>
2018-06-16 23:08:11 -0500

    Arreglé la búsqueda de pokemones

--- action.js
+++ action.js
@@ -16,16 +16,16 @@ function pokemon(){
 
   function buscarpokemones() {
     var content = "";
-    var search = document.getElementById("quien").value;  
-    var filtered = pokemones.filter(pokemones => (pokemones.name.toLowerCase().includes(search))|| (pokemones.id.includes(search)) || (pokemones.type.includes(search)) )
+    var search = document.getElementById("search").value;  
+    var filtered = pokemones.filter(pokemon => (pokemon.name.toLowerCase().includes(search))|| (pokemon.id.includes(search)) || (pokemon.type.includes(search)) )
     console.log(filtered)
 
     for (var i=0; i<filtered.length; i++){
     content += "<div class='pokemonItem'>";
-    content += "<img id='pok' src= 'img/photos/" + pokemones[i].photo + "'>";
-    content += "<h2>" + pokemones[i].name + "</h2>";
-    content += "<h1>" + pokemones[i].id + "</h1>";
-    content += "<img class= 'rounded float-right 'id='tipos'src= 'img/types/" + pokemones[i].type + "'>";
+    content += "<img id='pok' src= 'img/photos/" + filtered[i].photo + "'>";
+    content += "<h2>" + filtered[i].name + "</h2>";
+    content += "<h1>" + filtered[i].id + "</h1>";
+    content += "<img class= 'rounded float-right 'id='tipos'src= 'img/types/" + filtered[i].type + "'>";
     content += "</div>"
   }
   document.getElementById("result").innerHTML = content;
@@ -35,33 +35,8 @@ function pokemon(){
 
 
 
-// function mostrarpoke(quien){
-//   var result = "";
-
-//   if(quien==null){
-//     for(var i=0; i<pokemones.length; i++){
-//       result += pokemones[i].name;
-      
-//       // result += "<h1>" + pokemones[i].id + "</h1>";
-//       console.log(result)
-//     }
-//     document.getElementById("result").innerHTML = result;
-//   } else {
-//     for(var i=0; i<pokemones.length; i++){
-//       if(pokemones[i].name.toLowerCase().search(quien) >= 0){
-//         result += pokemones[i].name;  
-//       }
-//     }
-//     document.getElementById("result").innerHTML = result;
-//   }
-// }
-
-
-//  function busca(pokemones){
-//    var quien = document.getElementById("quien").value;
-
-//    mostrarpoke();
-//  }
+  
+  
 
 
  
